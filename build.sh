0063e268568c0152c9a277d9675e5ef2842fbd2c
m3r3nix <m3r3nix@protonmail.com>
2026-06-14 21:33:08 +0200

    Combine nightly and release build scripts into one

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-# build-release.sh
+# build.sh
 #
 # Compiles a fully-static rtorrent binary inside an Alpine Linux container.
 #
 # Required environment variables:
 #   VERSION_NUM   rtorrent / libtorrent version without the leading 'v'
 #                 (e.g.  0.16.12)
+#   RTORRENT_SHA  Git commit hash for rtorrent in case of a nightly build (e.g. 1a2b3c4)
+#   LIBTORRENT_SHA Git commit hash for libtorrent in case of a nightly build (e.g. 1a2b3c4)
 #   ARCH          Output filename suffix that identifies the target CPU
 #                 (e.g.  amd64  or  arm64)
 # Optional environment variables:
@@ -15,25 +17,37 @@
 
 set -eux
 
-: "${VERSION_NUM:?VERSION_NUM must be set (e.g. 0.16.12)}"
+: "${VERSION_NUM:=}"
+: "${RTORRENT_SHA:=}"
+: "${LIBTORRENT_SHA:=}"
 : "${ARCH:?ARCH must be set (e.g. amd64 or arm64)}"
 : "${WITH_OPTION:=}"
 : "${SUFFIX:=}"
+
+if [ -z "${VERSION_NUM}" ] && { [ -z "${RTORRENT_SHA}" ] || [ -z "${LIBTORRENT_SHA}" ]; }; then
+    echo "VERSION_NUM must be set for release builds, or RTORRENT_SHA and LIBTORRENT_SHA must be set for nightly builds." >&2
+    exit 1
+fi
 
 # ---------------------------------------------------------------------------
 # 1. System packages
 # ---------------------------------------------------------------------------
 apk add --no-cache \
+    autoconf \
+    autoconf-archive \
+    automake \
+    brotli-static \
     build-base \
-    gawk \
-    pkgconf \
-    wget \
     cppunit-dev \
+    curl \
     curl-dev \
     curl-static \
-    brotli-static \
+    gawk \
+    gettext-dev \
+    git \
     libidn2-static \
     libpsl-static \
+    libtool \
     libunistring-dev \
     libunistring-static \
     ncurses-dev \
@@ -41,6 +55,7 @@ apk add --no-cache \
     nghttp2-static \
     openssl-dev \
     openssl-libs-static \
+    pkgconf \
     xmlrpc-c-dev \
     xmlrpc-c-static \
     zlib-dev \
@@ -53,11 +68,20 @@ apk add --no-cache \
 mkdir -p /build
 cd /build
 
-wget -q \
-    "https://github.com/rakshasa/rtorrent/releases/download/v${VERSION_NUM}/libtorrent-${VERSION_NUM}.tar.gz"
-tar xf "libtorrent-${VERSION_NUM}.tar.gz"
-cd "libtorrent-${VERSION_NUM}"
+# If VERSION_NUM is set, we download the source tarball for that version. This is the default behavior for release builds.
+# If VERSION_NUM is not set, we clone the git repository and checkout the specified commit. This is used for nightly builds that do not have a version tag.
+if [ -n "${VERSION_NUM}" ]; then
+    curl -fsSLO \
+        "https://github.com/rakshasa/rtorrent/releases/download/v${VERSION_NUM}/libtorrent-${VERSION_NUM}.tar.gz"
+    tar xf "libtorrent-${VERSION_NUM}.tar.gz"
+    cd "libtorrent-${VERSION_NUM}"
+else
+    git clone --filter=blob:none --single-branch https://github.com/rakshasa/libtorrent.git
+    cd libtorrent
+    git checkout "$LIBTORRENT_SHA"
+fi
 
+autoreconf -fi
 ./configure \
     --enable-aligned \
     --enable-static \
@@ -75,11 +99,18 @@ make install
 # ---------------------------------------------------------------------------
 cd /build
 
-wget -q \
-    "https://github.com/rakshasa/rtorrent/releases/download/v${VERSION_NUM}/rtorrent-${VERSION_NUM}.tar.gz"
-tar xf "rtorrent-${VERSION_NUM}.tar.gz"
-cd "rtorrent-${VERSION_NUM}"
+if [ -n "${VERSION_NUM}" ]; then
+    curl -fsSLO \
+        "https://github.com/rakshasa/rtorrent/releases/download/v${VERSION_NUM}/rtorrent-${VERSION_NUM}.tar.gz"
+    tar xf "rtorrent-${VERSION_NUM}.tar.gz"
+    cd "rtorrent-${VERSION_NUM}"
+else
+    git clone --filter=blob:none --single-branch https://github.com/rakshasa/rtorrent.git
+    cd rtorrent
+    git checkout "$RTORRENT_SHA"
+fi
 
+autoreconf -fi
 ./configure \
     ${WITH_OPTION} \
     --enable-static \
